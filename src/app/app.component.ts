6025cc6cfad51286bd5c12859f4524bdaa8b97d6
Muhammed Saber <muahmmedsaber167@gamil.com>
2017-07-05 15:09:52 +0200

    2nd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase/app';
+import {Observable} from 'rxjs/Observable';
+
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  user: Observable<firebase.User>;
+items: FirebaseListObservable<any[]>;
+msgVal: string = '';
+
+constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
+ this.items = af.list('/messages', {
+   query: {
+     limitToLast: 50
+   }
+ });
+
+ this.user = this.afAuth.authState;
+}
+
+login() {
+    this.afAuth.auth.signInAnonymously();
+}
+
+logout() {
+    this.afAuth.auth.signOut();
+}
+
+Send(desc: string) {
+    this.items.push({ message: desc});
+    this.msgVal = '';
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+// New imports to update based on AngularFire2 version 4
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBvOgfJBrozQMQLMr-DmPo9X7UFYGodwWs",
+      authDomain: "what-we-do-684df.firebaseapp.com",
+      databaseURL: "https://what-we-do-684df.firebaseio.com",
+      projectId: "what-we-do-684df",
+      storageBucket: "what-we-do-684df.appspot.com",
+      messagingSenderId: "775832769582"
+};
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
